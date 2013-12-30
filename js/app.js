98fc7696ef58d18cf644832ecb7e8e1a37ba2926
Gilberto Villa <ingilniero@gmail.com>
2013-12-30 16:21:33 -0600

    Add contacts data

--- js/app.js
+++ js/app.js
@@ -45,3 +45,16 @@ App.PRODUCTS = [
     price: 20
   }
 ];
+
+App.CONTACTS = [
+  {
+    name: 'Giamia',
+    about: 'Although Giamia cmae form a humble spark of lightning, he quickly grew to be a great craftsman, providing  all the warming instruments needed by those close to him.',
+    avatar: 'img/contacts/giamia.png'
+  },
+  {
+    name: 'Anostagia',
+    about: 'Knowing there was a need for it, Anostagia drew on her experience and spearheaded the Flint & Flame storefront. In addition to coding the site, she also creates a few products available in the store.',
+    avatar: 'img/contacts/anostagia.png'
+  }
+];
