229c9fe5163ff1218325d413024a64a5d781fec1
Gilberto Villa <ingilniero@gmail.com>
2013-12-30 11:45:00 -0600

    Add IndexController and some bind some attributes

--- js/app.js
+++ js/app.js
@@ -7,3 +7,11 @@ App.Router.map(function() {
   this.route('credits');
 });
 
+App.IndexController = Ember.Controller.extend({
+  productsCount: 6,
+  logo: 'img/logo.png',
+  time: function() {
+    return (new Date).toDateString();
+  }.property()
+});
+
